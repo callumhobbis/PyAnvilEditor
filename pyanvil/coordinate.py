86b58b96e0854bc293a431578b8f1e8a2695042a
Callum Hobbis <callum.hobbis@gmail.com>
2023-05-24 23:59:01 +1200

    feat: add biome coordinates for completeness

--- pyanvil/coordinate.py
+++ pyanvil/coordinate.py
@@ -15,6 +15,10 @@ class Coordinate(ABC):
         pass
 
     @abstractmethod
+    def to_biome_coordinate(self) -> 'BiomeCoordinate':
+        pass
+
+    @abstractmethod
     def to_chunk_coordinate(self) -> 'ChunkCoordinate':
         pass
 
@@ -34,11 +38,35 @@ class AbsoluteCoordinate(Coordinate):
     def to_absolute_coordinate(self) -> 'AbsoluteCoordinate':
         return self
 
+    def to_biome_coordinate(self) -> 'BiomeCoordinate':
+        return BiomeCoordinate(self.x // 4, self.y // 4, self.z // 4)
+
     def to_chunk_coordinate(self) -> 'ChunkCoordinate':
         return ChunkCoordinate(self.x // 16, self.z // 16)
 
     def to_region_coordinate(self) -> 'RegionCoordinate':
         return RegionCoordinate(self.x // (16 * 32), self.z // (16 * 32))
+
+
+class BiomeCoordinate(Coordinate):
+    def __init__(self, x: int = 0, y: int = 0, z: int = 0):
+        super().__init__(x=x, z=z)
+        self.y: int = y
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y, self.z))
+
+    def to_absolute_coordinate(self) -> 'AbsoluteCoordinate':
+        return AbsoluteCoordinate(x=self.x * 4, y=self.y * 4, z=self.z * 4)
+
+    def to_biome_coordinate(self) -> 'BiomeCoordinate':
+        return self
+
+    def to_chunk_coordinate(self) -> 'ChunkCoordinate':
+        return ChunkCoordinate(self.x // 4, self.z // 4)
+
+    def to_region_coordinate(self) -> 'RegionCoordinate':
+        return RegionCoordinate(self.x // (4 * 32), self.z // (4 * 32))
 
 
 class ChunkCoordinate(Coordinate):
@@ -50,6 +78,9 @@ class ChunkCoordinate(Coordinate):
 
     def to_absolute_coordinate(self) -> 'AbsoluteCoordinate':
         return AbsoluteCoordinate(x=self.x * 16, y=0, z=self.z * 16)
+
+    def to_biome_coordinate(self) -> BiomeCoordinate:
+        return BiomeCoordinate(x=self.x // 4, y=0, z=self.z // 4)
 
     def to_chunk_coordinate(self) -> 'ChunkCoordinate':
         return self
@@ -75,6 +106,9 @@ class RegionCoordinate(Coordinate):
 
     def to_absolute_coordinate(self) -> 'AbsoluteCoordinate':
         return AbsoluteCoordinate(x=self.x * 16 * 32, y=0, z=self.z * 16 * 32)
+
+    def to_biome_coordinate(self) -> 'BiomeCoordinate':
+        return BiomeCoordinate(x=self.x * 4 * 32, y=0, z=self.z * 4 * 32)
 
     def to_chunk_coordinate(self) -> 'ChunkCoordinate':
         return ChunkCoordinate(self.x * 32, self.z * 32)
